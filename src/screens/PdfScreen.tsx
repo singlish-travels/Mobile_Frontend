ed4371cb10950a7e60d6afcc196b14aea06205b6
Aravinda-HWK <118424430+Aravinda-HWK@users.noreply.github.com>
2023-08-23 19:12:02 +0530

    Change can be omitted

--- src/screens/PdfScreen.tsx
+++ src/screens/PdfScreen.tsx
@@ -1,57 +1,59 @@
 import {
-    SafeAreaView,
-    StyleSheet,
-    Text,
-    TextInput,
-    TouchableOpacity,
-    View,
-    Dimensions,
-    ScrollView
-  } from "react-native";
-  import React from "react";
-  import Spacing from "../constants/Spacing";
-  import FontSize from "../constants/FontSize";
-  import Colors from "../constants/Colors";
-  import { Ionicons } from "@expo/vector-icons";
-  import { NativeStackScreenProps } from "@react-navigation/native-stack";
-  import { RootStackParamList } from "../types";
-  import AppTextInput from "../components/AppTextInput";
-  import { TabsStackScreenProps } from "../navigators/TabNavigator";
-  import Pdf from "react-native-pdf";
-  import { RootStackScreenProps } from "../navigators/RootNavigator";
-  
-  type Props = NativeStackScreenProps<RootStackParamList, "Login">;
-  const PdfScreen = ({
-    navigation,
-    route: {
-      params: { id },
-    },
-  }: RootStackScreenProps<"Pdf">) => {
-    const PdfResource = {uri : 'https://www.africau.edu/images/default/sample.pdf',cache:true}
-    return (
-      <View style = {styles.container}>
-        <Pdf
-          trustAllCerts = {false}
-          source={PdfResource}
-          style={styles.pdf}
-          onLoadComplete={(numberOfPages, filePath) => {
-            console.log(`number of pages: ${numberOfPages}`)
-          }}
-        />
-      </View>
-    );
-  };
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  Dimensions,
+  ScrollView,
+} from "react-native";
+import React from "react";
+import Spacing from "../constants/Spacing";
+import FontSize from "../constants/FontSize";
+import Colors from "../constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../types";
+import AppTextInput from "../components/AppTextInput";
+import { TabsStackScreenProps } from "../navigators/TabNavigator";
+import Pdf from "react-native-pdf";
+import { RootStackScreenProps } from "../navigators/RootNavigator";
 
-  
-  export default PdfScreen;
-  
-  const styles = StyleSheet.create({
-    container : {
-      flex:1
-    },
-    pdf:{
-      flex:1,
-      width: Dimensions.get('window').width,
-      height: Dimensions.get('window').height,
-    }
-  });
+type Props = NativeStackScreenProps<RootStackParamList, "Login">;
+const PdfScreen = ({
+  navigation,
+  route: {
+    params: { id },
+  },
+}: RootStackScreenProps<"Pdf">) => {
+  const PdfResource = {
+    uri: "https://firebasestorage.googleapis.com/v0/b/profile-image-1c78a.appspot.com/o/pdf%2FFlowcharts.pdf88a19bd5-5571-4b2f-95a3-13832e1ddfb2?alt=media&token=22e62d2d-3932-4463-ace6-49f1a0612989",
+    cache: true,
+  };
+  return (
+    <View style={styles.container}>
+      <Pdf
+        trustAllCerts={false}
+        source={PdfResource}
+        style={styles.pdf}
+        onLoadComplete={(numberOfPages, filePath) => {
+          console.log(`number of pages: ${numberOfPages}`);
+        }}
+      />
+    </View>
+  );
+};
+
+export default PdfScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  pdf: {
+    flex: 1,
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+});
--- src/screens/PdfViewer.tsx
+++ src/screens/PdfViewer.tsx
@@ -6,7 +6,7 @@ import {
   TouchableOpacity,
   View,
   Dimensions,
-  ScrollView
+  ScrollView,
 } from "react-native";
 import React from "react";
 import Spacing from "../constants/Spacing";
@@ -21,16 +21,19 @@ import Pdf from "react-native-pdf";
 
 type Props = NativeStackScreenProps<RootStackParamList, "Login">;
 
-const PdfViewer= ({ navigation }: TabsStackScreenProps<"Pdf">) => {
-  const PdfResource = {uri : 'https://www.africau.edu/images/default/sample.pdf',cache:true}
+const PdfViewer = ({ navigation }: TabsStackScreenProps<"Pdf">) => {
+  const PdfResource = {
+    uri: "https://firebasestorage.googleapis.com/v0/b/profile-image-1c78a.appspot.com/o/pdf%2FFlowcharts.pdf88a19bd5-5571-4b2f-95a3-13832e1ddfb2?alt=media&token=22e62d2d-3932-4463-ace6-49f1a0612989",
+    cache: true,
+  };
   return (
-    <View style = {styles.container}>
+    <View style={styles.container}>
       <Pdf
-        trustAllCerts = {false}
+        trustAllCerts={false}
         source={PdfResource}
         style={styles.pdf}
         onLoadComplete={(numberOfPages, filePath) => {
-          console.log(`number of pages: ${numberOfPages}`)
+          console.log(`number of pages: ${numberOfPages}`);
         }}
       />
     </View>
@@ -40,12 +43,12 @@ const PdfViewer= ({ navigation }: TabsStackScreenProps<"Pdf">) => {
 export default PdfViewer;
 
 const styles = StyleSheet.create({
-  container : {
-    flex:1
+  container: {
+    flex: 1,
   },
-  pdf:{
-    flex:1,
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  }
+  pdf: {
+    flex: 1,
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
 });

7116bb7a642b1eccfd29fd77ab2946e13a66ac69
Aravinda-HWK <118424430+Aravinda-HWK@users.noreply.github.com>
2023-09-25 11:43:30 +0530

    create build for mobile app

--- src/config/baseURL.ts
+++ src/config/baseURL.ts
@@ -1,7 +1,7 @@
 // config.ts
-const ip_address: string = "10.10.12.7";
+const ip_address: string = "192.168.8.122";
 const PORT = 3001;
 
-const baseURL: string = `http://${ip_address}:${PORT}`;
+const baseURL: string = "https://areader3d-be8a3a856279.herokuapp.com";
 
 export default baseURL;

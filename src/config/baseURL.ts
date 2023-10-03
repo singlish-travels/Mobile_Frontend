19afaf2347d037354d563fef5c645bee0a577593
Chinthani Chandeepa <119700964+Chinthanii@users.noreply.github.com>
2023-10-03 23:00:25 +0530

    Updated OCR page

--- src/config/baseURL.ts
+++ src/config/baseURL.ts
@@ -1,8 +1,9 @@
 // config.ts
-const ip_address: string = "192.168.8.122";
+const ip_address: string = "10.10.9.30";
 const PORT = 3001;
 
-// const baseURL: string = `http://${ip_address}:${PORT}`;
+//const baseURL: string = `http://${ip_address}:${PORT}`;
 const baseURL: string = "https://areader3d-be8a3a856279.herokuapp.com";
 
 export default baseURL;
+

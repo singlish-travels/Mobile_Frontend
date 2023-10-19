40e3866b22eccb2d4e5d73d679707576e4ea6abd
Chinthani Chandeepa <119700964+Chinthanii@users.noreply.github.com>
2023-10-19 11:01:20 +0530

    updated PaymentScreen

--- src/screens/PaymentScreen/PaymentScreen.tsx
+++ src/screens/PaymentScreen/PaymentScreen.tsx
@@ -18,7 +18,7 @@ import getPublisher from "../../api/profile/get_user";
 import updateReader from "../../api/profile/update_user";
 import jwt from "jwt-decode";
 import AsyncStorage from '@react-native-async-storage/async-storage';
-
+import { Padding } from "@mui/icons-material";
 
 
 
@@ -66,15 +66,128 @@ const PaymentScreen = ({ navigation }: TabsStackScreenProps<"Profile">) => {
     };
   
     return (
-      <SafeAreaView>
-        <View>
+      <SafeAreaView style={styles.container}>
+        <View style = {styles.topicContainer}>
+          <Text style={styles.topicText}>Payment Invoice</Text>
+          </View>
+          <View style={{flexDirection:"row",margin:30,height:50}}>
+            <View>
+                    <Image
+            source={require('../../assets/images/discover.png')} 
+            style={styles.image}
+          />
+          </View>
+          <View>
+                    <Image
+            source={require('../../assets/images/mastercard.png')} 
+            style={styles.image}
+          />
+          </View>
+          <View>
+                    <Image
+            source={require('../../assets/images/paypal.png')} 
+            style={styles.image}
+          />
+          </View>
+          <View>
+                    <Image
+            source={require('../../assets/images/visa.png')} 
+            style={styles.image}
+          />
+          </View>
+          </View>
 
-        </View>
+
+<View style={{width:"100%"}}>
+        <View style={styles.inputContainer}>
+                <Text style={styles.label}>Name on card</Text>
+                <TextInput
+                  style={styles.input}
+                  keyboardType="numeric"
+                  value={invoiceNumber}
+                  onChangeText={(text) => setInvoiceNumber(text)}
+                />
+              
+              </View>
+
+              <View style={styles.inputContainer}>
+                <Text style={styles.label}>Card number</Text>
+                <TextInput
+                  style={styles.input}
+                  keyboardType="numeric"
+                  value={invoiceNumber}
+                  onChangeText={(text) => setInvoiceNumber(text)}
+                />
+              
+              </View>
+              <View style={{flexDirection:"row"}}>
+              <View style={[styles.inputContainer, { flex: 1 ,marginRight:0,paddingRight:0}]}>
+                <Text style={styles.label}>Expiry date</Text>
+                <TextInput
+                  style={styles.input}
+                  keyboardType="numeric"
+                  placeholder="MM/YY"
+                  value={invoiceNumber}
+                  onChangeText={(text) => setInvoiceNumber(text)}
+                />
+                </View>
+                <View style={[styles.inputContainer, { flex: 1 ,marginLeft:0,paddingLeft:0}]}>
+                <Text style={styles.label}>Security code</Text>
+                <TextInput
+                  style={styles.input}
+                  keyboardType="numeric"
+                  value={invoiceNumber}
+                  onChangeText={(text) => setInvoiceNumber(text)}
+                />
+                </View>
+
+              </View>
+              <View style={styles.inputContainer}>
+                <Text style={styles.label}>ZIP/Postal code</Text>
+                <TextInput
+                  style={styles.input}
+                  keyboardType="numeric"
+                  value={invoiceNumber}
+                  onChangeText={(text) => setInvoiceNumber(text)}
+                />
+              
+              </View>
+
+              <TouchableOpacity
+        style={styles.payNowButton}
+        onPress={handlePayment}
+      >
+        <Text style={styles.payNowButtonText}>Pay Now</Text>
+      </TouchableOpacity>
+
+      </View>
+
+
       </SafeAreaView>
     );
   };
   
   const styles = StyleSheet.create({
+    topicContainer: {
+      width: "100%",
+      height: "auto",
+      backgroundColor: "#000030",
+      justifyContent: "flex-start",
+      marginTop: 50,
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 5,
+      },
+      shadowOpacity: 0.2,
+      shadowRadius: 4,
+      elevation: 5,
+    },
+    topicText: {
+      fontSize: 30,
+      color: "white",
+      alignSelf: "center",
+    },
     container: {
       flex: 1,
       justifyContent: 'center',
@@ -85,13 +198,35 @@ const PaymentScreen = ({ navigation }: TabsStackScreenProps<"Profile">) => {
       fontSize: 24,
       marginBottom: 20,
     },
+    image: {
+      margin:10,
+      marginBottom: 20,
+    },
+    inputContainer: {
+      margin: 4,
+      padding:8,
+    },
+    label: {
+      fontSize: 16,
+      marginBottom: 5,
+    },
     input: {
       height: 40,
       borderColor: 'gray',
       borderWidth: 1,
-      marginBottom: 10,
       padding: 10,
       width: '100%',
+    },
+    payNowButton: {
+      marginTop:20,
+      backgroundColor: '#3498db', // Button background color
+      padding: 15,
+      borderRadius: 5, // Rounded corners
+    },
+    payNowButtonText: {
+      fontSize: 18,
+      color: 'white', // Button text color
+      textAlign: 'center',
     },
   });
   

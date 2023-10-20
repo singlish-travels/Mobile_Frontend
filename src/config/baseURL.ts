ed92d06dbb66f3ef0f38bb27adeccdef9cea4b19
Aravinda-HWK <118424430+Aravinda-HWK@users.noreply.github.com>
2023-10-20 19:40:02 +0530

    Add changes to the baseURL

--- src/config/baseURL.ts
+++ src/config/baseURL.ts
@@ -1,8 +1,8 @@
 // config.ts
-const ip_address: string = "10.10.18.178";
+const ip_address: string = "192.168.137.1";
 const PORT = 3001;
 
-const baseURL: string = `http://${ip_address}:${PORT}`;
-//const baseURL: string = "https://areader3d-be8a3a856279.herokuapp.com";
+// const baseURL: string = `http://${ip_address}:${PORT}`;
+const baseURL: string = "https://areader3d-be8a3a856279.herokuapp.com";
 
 export default baseURL;
